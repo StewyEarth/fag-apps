dae53be8a78ef5c9a3ef5faf398dcfd5b25fcfd5
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-09-10 11:08:31 +0200

    It works again!

--- exempler/Stopwatch/App.js
+++ exempler/Stopwatch/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, } from 'react-native';
 
 import Counter from "./components/Counter"
 
--- exempler/Stopwatch/components/Counter.js
+++ exempler/Stopwatch/components/Counter.js
@@ -1,34 +1,25 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, Button } from 'react-native'
+import { Text, View, StyleSheet, Button,Icon } from 'react-native'
 
 export default class Counter extends Component {
-    constructor() {
-        super();
-        this.timer = {
-            isOn: false,
-            timer: null,
-            text: "Start"
-        };
-    };
 
     state = {
         seconds: 0,
-        minutes: 0
+        minutes: 0,
+        isOn: false,
+        timer: null,
     };
-
-    componentDidMount() {
-
-    };
+    
     resetTimer() {
         this.setState({ minutes: 0, seconds: 0 });
-        this.timer.isOn = "true";
+        this.setState({ isOn: true })
         this.startstopTimer();
     };
+
     startstopTimer() {
-        if (!this.timer.isOn) {
-            this.timer.isOn = true;
-            this.timer.text = "Stop";
-            this.timer.timer = setInterval(() => {
+        if (!this.state.isOn) {
+            this.setState({ isOn: true })
+            this.state.timer = setInterval(() => {
                 if (this.state.seconds >= 59) {
                     this.setState({ minutes: this.state.minutes + 1 });
                     this.setState({ seconds: 0 });
@@ -37,9 +28,8 @@ export default class Counter extends Component {
                 };
             }, 1000);
         } else {
-            this.timer.isOn = false;
-            this.timer.text = "Start";
-            clearInterval(this.timer.timer);
+            this.setState({ isOn: false })
+            clearInterval(this.state.timer);
         };
     };
 
@@ -47,6 +37,9 @@ export default class Counter extends Component {
         const { color, size } = this.props;
         let { minutes } = this.state;
         let { seconds } = this.state;
+
+        const btnText = this.state.isOn ? "Pause" : "Start"
+        const btnColor = this.state.isOn ? "orangered" : "green"
 
         if (minutes < 10) {
             minutes = "0" + minutes;
@@ -60,11 +53,12 @@ export default class Counter extends Component {
                     {minutes} : {seconds}
                 </Text>
                 <View style={styles.fixToText}>
-                    <Button
-                        title={this.timer.text}
+                    <Button color={btnColor}
+                        title={btnText}
                         onPress={() => this.startstopTimer()}
                     />
-                    <Button
+
+                    <Button color="red"
                         title="Reset"
                         onPress={() => this.resetTimer()}
                     />
